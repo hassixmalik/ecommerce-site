0e2af4346dca1e4d6b04a1152df84f04e102f69d
Hassan <hassi.x.malik@gmail.com>
2023-10-07 12:25:18 +0500

    Added Shop page

---
+++ application/controllers/Main.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+	
+    public function __construct()
+    {
+        parent::__construct();
+        //$this->load->model('user_model');
+        //$this->load->library('session');
+        //$this->load->database();
+    }
+
+    public function shop(){
+        $this->load->view('shop');
+    }
+}
